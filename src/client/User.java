6d25718121b74249a9a366088c3545c21913ce5e
Geyza9 <Geyza9@users.noreply.github.com>
2019-11-20 16:50:30 +0100

    Revert "kklh"
    This reverts commit 838927fc88def145b917dcf49272186acca4df39.

--- src/client/User.java
+++ src/client/User.java
@@ -3,12 +3,7 @@ package client;
 public class User {
 
     int id;
-    boolean ownClient;
-
-    public User(int id, boolean ownClient) {
-        this.id = id;
-        this.ownClient = ownClient;
-    }
+    boolean client;
 
 
 }
--- src/client/main.java
+++ src/client/main.java
@@ -1,46 +1,11 @@
 package client;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class main {
     static ArrayList<Message> messages = new ArrayList<>();
-    static ArrayList<User> users = new ArrayList<>();
 
-    //FUNCTIONS
-    private static void sendMessage(String message){
-        messages.add(new Message(message));
-        //send the message to the server
-    }
-
-    private static void receiveMessage(String message, int id){
-        messages.add(new Message(message,id));
-    }
-
-    private static void requestUserList(){
-        //fill users arraylist with users from server
-        for (int i = 0; i < 2 ; i++) {
-            users.add(new User(i,false));
-        }
-        users.add(new User(2,true));
-    }
-
-
-    //MAIN
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        boolean connected;
-
-        //try to connect
-        //run this if connection is established
-        connected = true;
-        //add users to list based on server
-        if(connected){
-            requestUserList();
-        }
-
-
-        System.out.println(Integer.toString(users.size()));
 
 
 
@@ -50,7 +15,8 @@ public class main {
 
 
 
-
-
+    void sendMessage(String message){
+        messages.add(new Message(message));
+    }
 
 }
