b29b58c9e536450e50321d555a1287562b322d9c
Thomas <bendixn@outlook.dk>
2019-11-20 15:39:45 +0100

    Revert "class chat comment"
    This reverts commit b07b34f1d5da6b1d2cb3fd7d364f76fdf0154a81.

--- src/client/chat.java
+++ src/client/chat.java
@@ -4,13 +4,9 @@ import javax.swing.*;
 
 public class chat {
     public static void main(String args[]) {
-        // creating the window for the chat
         JFrame frame = new JFrame("Chat");
-        // exit operation
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // size of the screen
         frame.setSize(300, 450);
-        // add button to send the message
         JButton sendButton = new JButton("Send");
         frame.getContentPane().add(sendButton);
         frame.setVisible(true);
