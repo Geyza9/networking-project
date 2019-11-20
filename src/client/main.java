446c5e1f313b6f823613546ede6cecfb96ec4819
Geyza9 <Geyza9@users.noreply.github.com>
2019-11-20 16:56:25 +0100

    merging
    merging of initial client functions with initial GUI

--- src/client/main.java
+++ src/client/main.java
@@ -2,6 +2,9 @@ package client;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+
 
 public class main {
     static ArrayList<Message> messages = new ArrayList<>();
@@ -38,6 +41,25 @@ public class main {
         if(connected){
             requestUserList();
         }
+
+        JFrame frame = new JFrame("Chat");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 500);
+
+        // bottom panel with a text field to chat in
+        JPanel chat = new JPanel();
+        JTextField text = new JTextField(10); // accepts up to 10 characters
+        JButton sendButton = new JButton("Send");
+        chat.add(text);
+        chat.add(sendButton);
+
+        // where the messages received will be shown
+        JLabel clientChat = new JLabel("");
+
+        // added the components to the frame
+        frame.getContentPane().add(BorderLayout.SOUTH, chat);
+        frame.getContentPane().add(BorderLayout.NORTH, clientChat);
+        frame.setVisible(true);
 
 
         System.out.println(Integer.toString(users.size()));
